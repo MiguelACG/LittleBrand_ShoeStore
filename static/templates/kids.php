c0865b8c741a2c418ee5a6f867682bae122a8859
Viki <Viki@users.noreply.github.com>
2020-11-30 17:14:22 -0500

    Details page (kids page)
    Ugly html cause styles didn't work in a separate .css file but fuck it

---
+++ static/templates/kids.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <!--Louboutin Store-->
+        <title>Contact</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../styles/mainPages.css">        
+    </head>
+        
+
+    <body>
+    <div>
+        <div class="detailsContainer" style="margin-left:10%; margin-right:10%;">
+            <div class="detailsHeader" style="margin: 0 0 40px 0;padding: 30px 5px 5px 5px; border-bottom: 1px solid #dededd; line-height: 1.4px;">
+                <htag1>
+                    <span class="detailsSpan" style="background: #FFF; display: inline-block; padding-right: 15px; font-weight: normal; font-size: 24px; text-transform: uppercase; letter-spacing: 2px;">
+                        <span class="title title-cap" role="heading" aria-level="1"
+                            style="font-size: 14px; font-weight: bold; color:black;">Dandelion</span>
+                    </span>
+                </htag1>
+            </div>
+            <div id="product" style="overflow:hidden;">
+                <section class="detailsProductMain" style="width: 50%;
+                        float: left;
+                        margin: 0 auto;
+                        position: relative;
+                        display: block; font-size: 13px;
+                        padding-bottom:100%; margin-bottom:-100%;">
+                    <div class="swiper-container gallery-top swiper-container-horizontal" 
+                        style="display:block; position: relative; overflow: hidden; z-index: 1;">
+                        <div class="swiper-wrapper" 
+                            style=" position: relative;
+                                width: 100%;
+                                height: 100%;display:flex;">
+                            <img class="swiper-slide swiper-slide-duplicate" data-width="1200" 
+                                data-height="1200" data-src="" src="https://s7d9.scene7.com/is/image/HoltRenfrew1/m_5000143940_01" alt-data-swiper-slide-index="0"
+                                style="width:70%; margin-left:auto; margin-right:auto; position:relative;">
+                        </div>
+                    </div>
+                </section>
+                <section class="product-sidebar"
+                        style="width: 50%;
+                            margin-left: 0;position: relative;
+                            float: right;    display: block; height:100%;">
+                    <div class="details" style="display: block;
+                        padding-bottom:100%; margin-bottom:-100%;">
+                        <div class="primary-details"
+                            style="position: relative; display:block;
+                                margin-bottom: 0px;
+                                padding: 0 0 20px 0;"   >
+                            <span id="recently-viewed-info" class="no-display" 
+                                recently-view-name="DANDELION" recently-view-url="http://us.christianlouboutin.com/ca_en/shop/men/dandelion-flat.html" 
+                                recently-view-image="https://images.us.christianlouboutin.com/media/catalog/product/cache/1/small_image/50x50/9df78eab33525d08d6e5fb8d27136e95/1/1/5/0/christianlouboutin-dandelion-1150564_BK01_1_1200x1200_1545133916_1.jpg"
+                                style="display: none !important;">
+                            </span>
+                            <span class="product-name title-cap"
+                                style="display: block;
+                                    width: 100%;
+                                    color: #000;
+                                    font-weight: 400;
+                                    font-size: 1.4em;
+                                    text-align: center;
+                                    margin: 15px 0 8px 0;text-transform: capitalize !important;">dandelion</span>
+                            <div class="price-box" style="display: block;">
+                                <span class="regular-price" id="product_id" style="display: block;">
+                                    <span class="price" 
+                                        style="font-size: 18px; width: 100%;
+                                            display: block;
+                                            text-align: center;
+                                            top: 15px;    font-weight: lighter;color: #222;">C$ 1,456.00</span>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="description" style="border-top: 1px dotted #d0d0d0;
+                            border-bottom: 1px dotted #d0d0d0;
+                            font-weight: normal;
+                            font-size: 11px;
+                            text-align: center;
+                            padding: 20px;
+                            margin: 15px 0;
+                            position: relative;
+                            line-height: 17px;
+                            letter-spacing: 0.0225em;">
+                            djhdbsgfjddjchvdsjhdsvdsv!
+                        </div>
+                        <a data-gtm-event-click="{&quot;event&quot;:&quot;gtm.ext.event&quot;,&quot;eventTmp&quot;:&quot;enhancedEcommerce&quot;,&quot;eventCategory&quot;:&quot;enhancedEcommerce&quot;,&quot;eventAction&quot;:&quot;addToWishlist&quot;,&quot;eventLabel&quot;:&quot;Dandelion&quot;,&quot;ecommerce&quot;:{&quot;currencyCode&quot;:&quot;CAD&quot;,&quot;add&quot;:{&quot;products&quot;:[{&quot;name&quot;:&quot;Dandelion&quot;,&quot;id&quot;:&quot;1150564BK01&quot;,&quot;price&quot;:&quot;1095.00&quot;,&quot;dimension2&quot;:&quot;black&quot;,&quot;category&quot;:&quot;men shoes&quot;}]}}}" 
+                            data-label="Add To Wishlist" data-action="Product Page" data-category="Product Page" 
+                            href="https://us.christianlouboutin.com/ca_en/wishlist/index/add/product/337065/form_key/uu2L16tsHt9InAbe/" 
+                            class="addToWishlist" onclick="productAddToCartForm.submitLight(this, 'https://us.christianlouboutin.com/ca_en/wishlist/index/add/product/337065/form_key/uu2L16tsHt9InAbe/'); return false;"
+                            style="display: block;
+                                position: relative;
+                                border-top: 1px dotted #D8D8D8;
+                                border-bottom: 1px dotted #D8D8D8;
+                                padding: 10px 0;
+                                margin-bottom: 20px;
+                                text-align: center;
+                                height: 20px;
+                                color: #ed1e24;
+                                font-weight: bold;
+                                letter-spacing: 2px;text-decoration: none;">
+                            Add to My Wishlist
+                        </a>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    </body>
+    </html>
