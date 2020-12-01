002651917b740d7fd8ffa9827856f17712ab9dbb
Viki <Viki@users.noreply.github.com>
2020-12-01 18:40:49 -0500

    LogIn/SignUp modals

--- static/templates/navbar.php
+++ static/templates/navbar.php
@@ -25,10 +25,77 @@
                 -->
             </div>
         </div>
+
         <!--Modify Href for login and cart screen-->
-        <a href="#" class="nav-link">Login</a>
+        <a href="#signInModal" class="nav-link" data-toggle="modal">Login</a> /
+        <a href="#signUpModal" class="nav-link" data-toggle="modal">SignUp</a>
         <a href="#" class="nav-link mt-1"><i class="fa fa-search"></i></a>
         <span id="iiyli">Cart</span>
         <!-- Number of times in cart -> Redirect-->
         <a data-toggle="modal" data-target="#cart" class="nav-link"><span id="id112">0</span></a>
         </nav>
+
+
+        <!--Sign in Modal-->
+        <div id="signInModal" class="modal fade">
+            <div class="modal-dialog modal-login">
+                <div class="modal-content">
+                    <div class="modal-header">			
+                        <h4 class="modal-title" style="margin-left:auto; margin-right:auto;">Member Login</h4>	
+                        <button type="button" style="margin-left:0;" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="/examples/actions/confirmation.php" method="post">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="username" placeholder="Username" required="required">		
+                            </div>
+                            
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required="required">	
+                            </div>        
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn" style="float:left;">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <span style="margin-left:auto; margin-right:auto;">Don't have an account? <a href="#">Sign Up</a></span>
+                    <!--    <a href="#">Forgot Password?</a>-->
+                    </div>
+                </div>
+            </div>
+        </div>     
+
+        <!--Sign Up Modal-->
+        <div id="signUpModal" class="modal fade">
+            <div class="modal-dialog modal-login">
+                <div class="modal-content">
+                    <div class="modal-header">			
+                        <h4 class="modal-title" style="margin-left:auto; margin-right:auto;">Member Sign Up</h4>	
+                        <button type="button" style="margin-left:0;" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="/examples/actions/confirmation.php" method="post">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="firstname" placeholder="First Name" required="required" style="width:49%; display:inline-block;">
+                                <input type="text" class="form-control" name="lastname" placeholder="Last Name" required="required" style="width:50%; display:inline-block;">		
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="username" placeholder="Username" required="required">		
+                            </div>
+                            
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required="required">	
+                            </div>        
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn" style="float:left;">Sign Up</button>
+                            </div>
+                        </form>
+                    </div>
+                <!--    <div class="modal-footer">
+                        <span style="margin-left:auto; margin-right:auto;">Don't have an account? <a href="#">Sign Up</a></span>
+                       <a href="#">Forgot Password?</a>
+                    </div>-->
+                </div>
+            </div>
+        </div>
