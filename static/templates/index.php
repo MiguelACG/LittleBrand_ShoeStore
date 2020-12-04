89376cfa408f9d4724696c89ee65d4fc1d819474
MiguelACG <63476768+MiguelACG@users.noreply.github.com>
2020-12-03 19:47:04 -0500

    Database and connection
    Database and connection to database created

--- static/templates/index.php
+++ static/templates/index.php
@@ -19,10 +19,16 @@
 <body>
 
     <?php
+
+        $dbuser = 'root';
+        $dbpassword = '';
+        $db = 'littlebranddb';
+
+        $dataconnection = new mysqli('localhost', $dbuser, $dbpassword, $db) or die('Could not find host db');
+        
         include_once('navbar.php');
 
         $page_select = $_GET['page'];
-
         switch($page_select){
             case 'main': include_once('main.php'); break;
             case 'collections': include_once('collections.php'); break;
@@ -36,119 +42,6 @@
 
     ?>
 
-    <!--<nav class="navbar navbar-expand-lg navbar-light bg-light"><a href="#" class="navbar-brand d-lg-none">Gridbox</a><button type="button" data-toggle="collapse" data-target="#navigations-02"
-            class="navbar-toggler"><span class="navbar-toggler-icon"></span></button><a href="#" class="navbar-brand mr-0">Little Brand Shoe Store</a>
-        <div id="navigations-02" class="collapse navbar-collapse justify-content-md-center">
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item mr-2"></li><a href="#" class="nav-link active">Collections<span class="sr-only">(current)</span></a><a href="#" class="nav-link">Women</a><a href="#"
-                    class="nav-link">Men</a><a href="#" class="nav-link">Kids</a><a href="#" class="nav-link">Brands</a><a href="#" class="nav-link">Contact</a>
-                <li class="nav-item mr-2"></li>
-            </ul>
-        </div>
-        <div class="row w-100">
-            <div class="col-4 pl-lg-0">
-                <ul class="navbar-nav">
-                    <li class="nav-item"></li>
-                    <li class="nav-item"></li>
-                    <li class="nav-item"></li>
-                    <li class="nav-item cart"></li>
-                </ul>
-                <div class="col-4 pr-lg-0"></div>
-            </div>
-        </div><a href="#" class="nav-link">Login</a><a href="#" class="nav-link mt-1"><i class="fa fa-search"></i></a><span id="iiyli">Cart</span><a data-toggle="modal" data-target="#cart"
-            class="nav-link"><span id="id112">0</span></a>
-    </nav>-->
-    
-    <!-- <div class="bg-dark hero-form-1 overlay">
-         Main Content --
-        <div class="d-lg-flex align-items-lg-center py-5">
-            <div class="container space-2 space-0--lg mt-lg-8 mt-5">
-                <div class="row justify-content-lg-between align-items-lg-center mt-lg-5 mt-md-5">
-                    <div class="col-lg-5 mb-7 mb-lg-0">
-                        <-- Title --
-                        <h1 class="display-4 font-size-48--md-down text-white mb-0">Buy Simple. Buy Luxury</h1>
-                        <div class="row">
-                            <div class="col-5"></div>
-                            <div class="col-2"></div>
-                        </div>
-                        <ul class="list-unstyled text-light mt-5 mb-0"></ul>
-                    </div>
-                    <div class="col-lg-5">
-                        <-- Signup Form --
-                        <-- End Signup Form --
-                    </div>
-                </div>
-            </div>
-        </div><-- End Main Content --
-    </div>
-    <section class="py-10 bg-light">
-        <div class="container">
-            <h2 class="mb-4 pt-3 pb-3">New Arrivals</h2>
-            <div class="row">
-                <div class="col-xl-3 col-lg-4 col-md-6 mb-5"><a href="javascript:void(0);" class="card product-card-com h-100">
-                        <div class="card-tag card-tag-dark card-tag-top-right card-tag-lg">$956</div><img src="https://i.ebayimg.com/images/g/iIMAAOxy14VRRYuk/s-l300.jpg" alt="..."
-                            class="card-img-top" />
-                        <div class="card-body p-3">
-                            <div class="card-title small mb-0">Steve Madden</div>
-                        </div>
-                    </a></div>
-                <div class="col-xl-3 col-lg-4 col-md-6 mb-5"><a href="javascript:void(0);" class="card product-card-com h-100">
-                        <div class="card-tag card-tag-dark card-tag-top-right card-tag-lg">$500
-                        </div><img src="https://img-static.tradesy.com/item/22324255/louis-vuitton-monogram-cherie-pumps-size-eu-39-approx-us-9-regular-m-b-0-1-960-960.jpg" alt="..."
-                            class="card-img-top" />
-                        <div class="card-body p-3">
-                            <div class="card-title small mb-0">Louis Vuitton</div>
-                        </div>
-                    </a></div>
-                <div class="col-xl-3 col-lg-4 col-md-6 mb-5"><a href="javascript:void(0);" class="card product-card-com h-100">
-                        <div class="card-tag card-tag-dark card-tag-top-right card-tag-lg">$900
-                        </div><img src="https://luxity.co.za/wp-content/uploads/2019/05/YVES-SAINT-LAURENT-Patent-Leather-Opyum-Pumps-Black-S-41.5-7.5-side-1200x1200.jpg" alt="..."
-                            class="card-img-top" />
-                        <div class="card-body p-3">
-                            <div class="card-title small mb-0">Yves Saint Laurent</div>
-                        </div>
-                    </a></div>
-                <div class="col-xl-3 col-lg-4 col-md-6 mb-5"><a href="javascript:void(0);" class="card product-card-com h-100">
-                        <div class="card-tag card-tag-dark card-tag-top-right card-tag-lg">$405</div><img
-                            src="https://www.brownsshoes.com/dw/image/v2/BFTX_PRD/on/demandware.static/-/Sites-brownsshoes-master-catalog/default/dw9a11fafc/233676_1.jpg?sw=1920&sh=2170" alt="..."
-                            class="card-img-top" />
-                        <div class="card-body p-3">
-                            <div class="card-title small mb-0">Galizio Torresi</div>
-                        </div>
-                    </a></div>
-                <div class="col-xl-3 col-lg-4 col-md-6 mb-5"><a href="javascript:void(0);" class="card product-card-com h-100">
-                        <div class="card-tag card-tag-dark card-tag-top-right card-tag-lg">$854
-                        </div><img src="http://g02.a.alicdn.com/kf/HTB1KoLfKXXXXXbxXXXXq6xXFXXX4/2015-Italian-luxury-designer-dress-fashion-mens-genuine-leather-shoes-basic-flats-for-men-business-office.jpg" alt="..." class="card-img-top" />
-                        <div class="card-body p-3">
-                            <div class="card-title small mb-0">Salvatore Ferragamo</div>
-                        </div>
-                    </a></div>
-                <div class="col-xl-3 col-lg-4 col-md-6 mb-5"><a href="javascript:void(0);" class="card product-card-com h-100">
-                        <div class="card-tag card-tag-dark card-tag-top-right card-tag-lg">$562
-                        </div><img src="https://th.bing.com/th/id/OIP.UwFnfEzZuXj89tHpBPARNgHaHa?pid=Api&rs=1" alt="..." class="card-img-top" />
-                        <div class="card-body p-3">
-                            <div class="card-title small mb-0">Yves Saint Laurent</div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-md-6 mb-5"><a href="javascript:void(0);" class="card product-card-com h-100">
-                    <div class="card-tag card-tag-dark card-tag-top-right card-tag-lg">$745
-                    </div><img src="https://www.raffaello-network.com/item_images/guc03sho-428609d3vn0d3vn0-1060-large-3.jpg" alt="..." class="card-img-top" />
-                    <div class="card-body p-3">
-                        <div class="card-title small mb-0">Gucci</div>
-                    </div>
-                </a>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-md-6 mb-5"><a href="javascript:void(0);" class="card product-card-com h-100">
-                    <div class="card-tag card-tag-dark card-tag-top-right card-tag-lg">$625
-                    </div><img src="https://cdn.raffaello-network.com/english/fashion-details/358859/81/gucci-womens-shoes_gu0wsh-408208c9d001000-large-3.jpg" alt="..." class="card-img-top" />
-                    <div class="card-body p-3">
-                        <div class="card-title small mb-0">Gucci</div>
-                    </div>
-                </a>
-                </div>
-            </div>
-        </div>-->
         <footer class="footer pt-5 pb-5 py-5 mt-auto bg-light footer-light">
             <div class="container">
                 <div class="row">
